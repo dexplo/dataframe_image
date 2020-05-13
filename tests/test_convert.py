6019f93a2c9043be6a97fc80643da9475a68281a
tdpetrou <petrou.theodore@gmail.com>
2020-05-13 11:38:12 -0400

    md tests

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -3,11 +3,30 @@ import nbformat
 
 class TestPDF:
 
-    filename = 'notebooks/Test_1 Notebook.ipynb'
+    filename = 'notebooks/Test 1 Notebook.ipynb'
 
     def test_same_folder(self):
         convert(self.filename, to='pdf')
 
     def test_different_folder(self):
-        convert(self.filename, to='pdf', output_dir='test_output', save_notebook=True, 
-                document_name='New Test Name')
+        convert(self.filename, to='pdf', output_dir='notebooks/test_output', save_notebook=True, 
+                document_name='Test 1 Notebook NEW NAME')
+
+    def test_no_execute(self):
+        convert(self.filename, to='pdf', document_name='Test 1 Notebook NO EXECUTE')
+
+
+class TestMarkdown:
+
+    filename = 'notebooks/Test 1 Notebook.ipynb'
+
+    def test_same_folder(self):
+        convert(self.filename, to='md')
+
+    def test_different_folder(self):
+        convert(self.filename, to='md', output_dir='notebooks/test_output', 
+                save_notebook=True, document_name='New Test Name')
+
+    def test_no_execute(self):
+        convert(self.filename, to='md', document_name='Test 1 Notebook NO EXECUTE')
+
