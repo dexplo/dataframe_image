80af6dec2fa062cabde2aa6cb6f1b4d96bf02c26
tdpetrou <petrou.theodore@gmail.com>
2020-05-19 17:48:32 -0400

    latex options

--- dataframe_image/_latex_pdf.py
+++ dataframe_image/_latex_pdf.py
@@ -10,23 +10,23 @@ from ._preprocessors import MarkdownPreprocessor, NoExecuteDataFramePreprocessor
 td = TemporaryDirectory()
 td_path = Path(td.name)
 
-def get_latex_command(self, latex_command):
-        texs = ['xelatex', 'pdflatex', 'texi2pdf']
-        final_tex = ''
-        for tex in texs:
-            if shutil.which(tex):
-                final_tex = tex
-                break
-                
-        if not final_tex:
-            raise OSError('No latex installation found. '\
-                          'Try downloading as pdf via browser instead.\n'\
-                          'Find out how to install latex here: '\
-                          'https://nbconvert.readthedocs.io/en/latest/install.html#installing-tex')
-        latex_command = [final_tex, '{filename}']
-        if final_tex == 'xelatex':
-            latex_command.append('-quiet')
-        return latex_command
+def get_latex_command():
+    texs = ['xelatex', 'pdflatex', 'texi2pdf']
+    final_tex = ''
+    for tex in texs:
+        if shutil.which(tex):
+            final_tex = tex
+            break
+            
+    if not final_tex:
+        raise OSError('No latex installation found. '\
+                        'Try downloading as pdf via browser instead.\n'\
+                        'Find out how to install latex here: '\
+                        'https://nbconvert.readthedocs.io/en/latest/install.html#installing-tex')
+    latex_command = [final_tex, '{filename}']
+    if final_tex == 'xelatex':
+        latex_command.append('-quiet')
+    return latex_command
 
 class DataFramePDFExporter(PDFExporter):
  
