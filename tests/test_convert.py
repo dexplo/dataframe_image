bbd3805ac880361703baf2ea51edd45c2e977350
tdpetrou <petrou.theodore@gmail.com>
2020-07-26 11:01:10 -0400

    clean up tests

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -3,22 +3,32 @@ import pytest
 from dataframe_image import convert
 
 
-filenames = ['tests/notebooks/Short Test Notebook EXECUTED.ipynb',
-             'tests/notebooks/short notebook with md tables EXECUTED.ipynb']
+filenames = ['tests/notebooks/Short.ipynb',
+             'tests/notebooks/Test 1.ipynb',
+             'tests/notebooks/Test 1 EXECUTED.ipynb']
 tos = ['pdf', 'md']
+executes = [False, True]
 
 @pytest.mark.parametrize('filename', filenames)
 @pytest.mark.parametrize('to', tos)
+@pytest.mark.parametrize('execute', executes)
 class TestConvert:    
 
-    def test_same_folder(self, filename, to):
-        convert(filename, to=to)
+    def test_same_folder(self, filename, to, execute):
+        ex = ' executed' if execute else ' not_executed'
+        document_name = Path(filename).stem + ex + ' NEW NAME'
+        convert(filename, to=to, execute=execute, document_name=document_name)
 
-    def test_different_folder(self, filename, to):
-        name = Path(filename).name
-        convert(filename, to=to, output_dir='tests/test_output', save_notebook=True, 
-                document_name=f'{name} NEW NAME')
+class TestConvertOther:    
 
-    def test_execute(self, filename, to):
-        name = Path(filename).name
-        convert(filename, to=to, document_name=f'{name} EXECUTE', execute=True)
+    def test_save_notebook(self):
+        filename = 'tests/notebooks/Short.ipynb'
+        to = 'pdf'
+        document_name = Path(filename).stem + ' saved NEW NAME'
+        convert(filename, to=to, save_notebook=True, execute=True, document_name=document_name)
+
+    def test_output_dir(self):
+        filename = 'tests/notebooks/Short.ipynb'
+        to = 'pdf'
+        document_name = Path(filename).stem + ' output_dir NEW NAME'
+        convert(filename, to=to, output_dir='tests/test_output', document_name=document_name)
