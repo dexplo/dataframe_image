f39a077d92b0699b19ac9518ae34e3196e9440e0
tdpetrou <petrou.theodore@gmail.com>
2020-07-26 11:16:08 -0400

    separate pdf/md tests

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -6,18 +6,30 @@ from dataframe_image import convert
 filenames = ['tests/notebooks/Short.ipynb',
              'tests/notebooks/Test 1.ipynb',
              'tests/notebooks/Test 1 EXECUTED.ipynb']
-tos = ['pdf', 'md']
+uses = ['latex', 'browser']
 executes = [False, True]
 
-@pytest.mark.parametrize('filename', filenames)
-@pytest.mark.parametrize('to', tos)
-@pytest.mark.parametrize('execute', executes)
-class TestConvert:    
 
-    def test_same_folder(self, filename, to, execute):
+@pytest.mark.parametrize('filename', filenames)
+@pytest.mark.parametrize('use', uses)
+@pytest.mark.parametrize('execute', executes)
+class TestConvertPDF:    
+
+    def test_same_folder(self, filename, use, execute):
+        ex = ' executed' if execute else ' not_executed'
+        document_name = Path(filename).stem + ' ' + use +  ex + ' NEW NAME'
+        convert(filename, to='pdf', use=use, execute=execute, document_name=document_name)
+
+
+@pytest.mark.parametrize('filename', filenames)
+@pytest.mark.parametrize('execute', executes)
+class TestConvertMD:    
+
+    def test_same_folder(self, filename, execute):
         ex = ' executed' if execute else ' not_executed'
         document_name = Path(filename).stem + ex + ' NEW NAME'
-        convert(filename, to=to, execute=execute, document_name=document_name)
+        convert(filename, to='md', execute=execute, document_name=document_name)
+
 
 class TestConvertOther:    
 
@@ -32,3 +44,4 @@ class TestConvertOther:
         to = 'pdf'
         document_name = Path(filename).stem + ' output_dir NEW NAME'
         convert(filename, to=to, output_dir='tests/test_output', document_name=document_name)
+        
