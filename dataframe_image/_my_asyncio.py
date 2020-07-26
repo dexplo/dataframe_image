9a941425221aabffc9b1315c0a56969c70198890
tdpetrou <petrou.theodore@gmail.com>
2020-07-26 16:19:50 -0400

    add missing functions for 3.6

--- dataframe_image/_my_asyncio.py
+++ dataframe_image/_my_asyncio.py
@@ -3,8 +3,42 @@
 import asyncio
 
 
+def _get_loop(fut):
+    # Tries to call Future.get_loop() if it's available.
+    # Otherwise fallbacks to using the old '_loop' property.
+    try:
+        get_loop = fut.get_loop
+    except AttributeError:
+        pass
+    else:
+        return get_loop()
+    return fut._loop
+
+
+def all_tasks(loop=None):
+    """Return a set of all tasks for the loop."""
+    if loop is None:
+        loop = events.get_running_loop()
+    # Looping over a WeakSet (_all_tasks) isn't safe as it can be updated from another
+    # thread while we do so. Therefore we cast it to list prior to filtering. The list
+    # cast itself requires iteration, so we repeat it several times ignoring
+    # RuntimeErrors (which are not very likely to occur). See issues 34970 and 36607 for
+    # details.
+    i = 0
+    while True:
+        try:
+            tasks = list(_all_tasks)
+        except RuntimeError:
+            i += 1
+            if i >= 1000:
+                raise
+        else:
+            break
+    return {t for t in tasks
+            if _get_loop(t) is loop and not t.done()}
+
 def _cancel_all_tasks(loop):
-    to_cancel = asyncio.tasks.all_tasks(loop)
+    to_cancel = all_tasks(loop)
     if not to_cancel:
         return
 
