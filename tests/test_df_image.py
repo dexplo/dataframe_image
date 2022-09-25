6d54a852366358aebec206ccabd29841d44a83a9
hns258 <ncz258@gmail.com>
2022-09-25 08:41:02 -0400

    add parameterized tests for various dpi values

--- tests/test_df_image.py
+++ tests/test_df_image.py
@@ -7,19 +7,19 @@ df = pd.read_csv(
     "tests/notebooks/data/covid19.csv", parse_dates=["date"], index_col="date"
 )
 
+test_dpi_values = [50, 400, 600, 800]
 
 class TestImage:
     def test_df(self):
         df.tail(10).dfi.export("tests/test_output/covid19.png")
 
+    @pytest.mark.parametrize('dpi', test_dpi_values)
+    def test_chrome_changed_dpi(self, dpi):
+        df.tail(10).dfi.export(f"tests/test_output/covid19_dpi_{dpi}.png", dpi)
+
     def test_styled(self):
         df.tail(10).style.background_gradient().export_png(
             "tests/test_output/covid19_styled.png"
-        )
-
-    def test_styled_higher_device_scale_factor(self):
-        df.tail(10).style.background_gradient().export_png(
-            "tests/test_output/covid19_styled_dpi_400.png", dpi=400
         )
 
     def test_mpl(self):
@@ -27,8 +27,8 @@ class TestImage:
             "tests/test_output/covid19_mpl.png", table_conversion="matplotlib"
         )
 
-    def test_mpl_higher_dpi(self):
-        test_dpi = 400
+    @pytest.mark.parametrize('dpi', test_dpi_values)
+    def test_mpl_changed_dpi(self, dpi):
         df.tail(10).dfi.export(
-            f"tests/test_output/covid19_mpl_dpi_{test_dpi}.png", table_conversion="matplotlib", dpi=test_dpi
+            f"tests/test_output/covid19_mpl_dpi_{dpi}.png", table_conversion="matplotlib", dpi=dpi
         )
