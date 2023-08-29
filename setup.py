cf3351dd5179f3ed0f11868184fe848cca2d0298
John Lyu <lvjunhong@citics.com>
2023-08-29 10:23:20 +0800

    add version file

--- setup.py
+++ setup.py
@@ -50,7 +50,8 @@ setuptools.setup(
     use_scm_version={
         'write_to': 'dataframe_image/_version.py',
         'write_to_template': '__version__ = "{version}"',
-        'tag_regex': r'^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$',
+        'local_scheme': 'node-and-date'
+        # 'tag_regex': r'^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$',
     },
     setup_requires=["setuptools_scm"],
 )
