b0bc592630f6cae04976e6eb49ab2237af48f1df
John Lyu <lvjunhong@citics.com>
2023-09-01 13:17:40 +0800

    ensure test output path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 import pytest
 
 
+@pytest.fixture(scope='session', autouse=True)
+def ensure_output_dir():
+    from pathlib import Path
+
+    output_dir = Path("tests/test_output")
+    output_dir.mkdir(if_exists=True, parents=True)
+
 @pytest.fixture()
 def document_name(request):
     return request.node.name.replace(" ", "_").replace("/", "_")
